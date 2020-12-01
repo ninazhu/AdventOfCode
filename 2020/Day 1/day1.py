8cd9b0e6d0a87d6f2dbacca816386f872e7a6220
Nina Zhu <ninazoo@gmail.com>
2020-12-01 16:25:28 -0500

    initial commit

---
+++ 2020/Day 1/day1.py
@@ -0,0 +1,37 @@
+# Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+goal = 2020
+
+def read_input(file_loc):
+    with open(file_loc, "r") as f:
+        values = f.read().split()
+    values = [int(i) for i in values]
+    return values
+
+def get_2_nums(values, goal):
+    for val in values:
+        if (goal - val) in values:
+            return [val, goal-val]
+    return None
+
+def get_3_nums(values):
+    orig_values = values
+    for val in orig_values:
+        values.remove(val)
+        sub_goal = goal - val
+        res = get_2_nums(values, sub_goal)
+        if res is not None:
+            return [val, res[0], res[1]]
+
+values = read_input("../InputData/day1.txt")
+res = get_2_nums(values, goal)
+num1 = res[0]
+num2 = res[1]
+print(f'Num 1: {num1} Num 2: {num2} Product: {num1 * num2}')
+
+res = get_3_nums(values)
+num1 = res[0]
+num2 = res[1]
+num3 = res[2]
+print(f'Num 1: {num1} Num 2: {num2} Num 3: {num3} Product: {num1 * num2 * num3}')
+
