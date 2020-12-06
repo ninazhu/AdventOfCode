3e15cb7d693714e91e4ea8cd5e60e4e57a1f13e2
Nina Zhu <ninazoo@gmail.com>
2020-12-06 00:23:47 -0500

    day 6 part 2'

--- 2020/Day6/day6.py
+++ 2020/Day6/day6.py
@@ -7,10 +7,17 @@ def read_input(file_loc):
     return values
 
 groups = read_input("../Inputs/day6.txt")
-total_count = 0
+total_count_1 = 0
+total_count_2 = 0
 for group in groups:
     total_ans = group.replace('\n', '')
     unique_ans = set(list(total_ans))
-    total_count = total_count + len(unique_ans)
+    total_count_1 = total_count_1 + len(unique_ans)
 
-print(f'Part 1:\nTotal count: {total_count}')
+    # for part 2
+    individuals = [list(x) for x in group.split('\n')]
+    overlap = len(set.intersection(*map(set, individuals)))
+    total_count_2 = total_count_2 + overlap
+
+print(f'Part 1:\nTotal count: {total_count_1}')
+print(f'Part 2:\nTotal count: {total_count_2}')
