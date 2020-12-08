edde4236c9c3329f6a81cb145280ee4f0cc29fcc
Nina Zhu <ninazoo@gmail.com>
2020-12-08 00:32:15 -0500

    day 8 part 2

--- 2020/Day8/day8.py
+++ 2020/Day8/day8.py
@@ -7,7 +7,9 @@ def read_input(file_loc):
 
 def get_next_instruction(instructions, ind, executed_indices, accum):
     if ind in executed_indices:
-        return accum
+        return accum, 1, executed_indices
+    elif ind >= len(instructions):    # Part 2
+        return accum, 0, executed_indices
     executed_indices.append(ind)
     instruction = instructions[ind].split(" ")
     if instruction[0] == "acc":
@@ -20,8 +22,24 @@ def get_next_instruction(instructions, ind, executed_indices, accum):
     return get_next_instruction(instructions, ind, executed_indices, accum)
 
 instructions = read_input("../Inputs/day8.txt")
-executed_indices = []
 accum = 0
 
-result = get_next_instruction(instructions, 0, executed_indices, 0)
-print(f'Part 1:\nAccumulator value: {result}')
+res, status, inds = get_next_instruction(instructions, 0, [], 0)
+print(f'Part 1:\nAccumulator value: {res}')
+
+# Part 2:
+for i in inds:
+    if instructions[i].split(" ")[0] == 'jmp':
+        instr_copy = instructions.copy()
+        instr_copy[i] = instructions[i].replace("jmp", "nop")
+        res, term_status, inds = get_next_instruction(instr_copy, 0, [], 0)
+        if term_status == 0:
+            print(f'Part 2:\nAccumulator value: {res}')
+            break
+    elif instructions[i].split(" ")[0] == 'nop':
+        instr_copy = instructions.copy()
+        instr_copy[i] = instructions[i].replace("nop", "jmp")
+        res, term_status, inds = get_next_instruction(instr_copy, 0, [], 0)
+        if term_status == 0:
+            print(f'Part 2:\nAccumulator value: {res}')
+            break
